38ae1821e269568d3d37e839cde4062c391e6ae4
may <maymehenni2004@gmail.com>
2024-01-06 11:06:46 +0100

    correction des erreures +ajout de commentaires

--- code.c
+++ code.c
@@ -32,7 +32,7 @@ Fichier ouvrir(char *nom, char *mode) {
 void recuperer_chaine( int n, int i, int *j, char *ch, Bloc *buf ) {
     int k=0;
     for (k = 0; k < n; k++) {
-        ch[k] = buf->tab[j];
+        ch[k] = buf->tab[*j];
         (*j)++;
         if (*j > b) {
             *j = 1
@@ -43,25 +43,29 @@ void recuperer_chaine( int n, int i, int *j, char *ch, Bloc *buf ) {
 
 }
 void Rech( char cle[20], char nomfichier[], int *trouv, int *i, int *j ) {
-    Fichier F = ouvrir(nomfichier, "rw");
+    Fichier F = ouvrir(nomfichier, "r");
     Bloc buf;
     *i = 1;
     lireBloc(&F, *i, &buf);
-    while ( *i < entete(&F,1) || *j != entete(&F,2) ) {
-        char chLog[3];
-          recuperer_chaine( 3, *i, j, chLong, &buf );
-char eff[1];
-    recuperer_chaine( 1, *i, j, eff, &buf );
-    char chCle[20];
-        recuperer_chaine( 20, *i, j, chCle, &buf );
-if ( strcmp(chCle, cle) = 0 || eff[0] == 'N' )
-trouv = 1;
+    while ( *i < entete(&F,1) || *j != entete(&F,2) ) { // i<denierBloc OU j<>posLibre
+        // a chaque itération, on traite un enreg
+        char chLong[3];
+        recuperer_chaine( 3, *i, j, chLong, &buf ); // récupère longueur de l'enreg
+        char eff[1];
+        recuperer_chaine( 1, *i, j, eff, &buf ); // le car d'effacement
+        char chCle[20];
+        recuperer_chaine( 20, *i, j, chCle, &buf ); // et la clé de l'enreg
+        if ( strcmp(chCle, cle) == 0 && eff[0] == 'N' )
+            *trouv = 1;
         else {
-        *j = *j + atoi(chLong);
-        *j = 1;
-        *i = buf.Suiv
-        lireBloc(&F, *i, &buf);}
-}
-
+            // on passe au suivant
+            *j = *j + atoi(chLong); // on a deja lu les 20 car de la cle
+            if ( *j > b ) {
+                *j = 1;
+                *i = buf.Suiv;
+                lireBloc(&F, *i, &buf);
+            }
+        }
+    }
     fermer(&F);
 }
