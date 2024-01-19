04ffdc9dfec975761d8362a380783f0d46f7291b
Ahleeem <150480223+Ahleeem@users.noreply.github.com>
2024-01-19 13:16:15 +0100

    Update code.c

--- code.c
+++ code.c
@@ -225,4 +225,23 @@ void libererBloc(Fichier *f, int i) {
     affecterEntete(f, 1, i);      // Le nouveau premier bloc libre est le bloc libéré
 }
 
+void creation_fichier(Fichier *fichier,int n)
+{
+    int k=0, cle;
+    char *info=malloc(sizeof(char)*50);
+    aff_entete(fichier,1,1);     //nbr totale de bloc allouer =1
+
+    for(k=0;k<n;k++)
+    {
+        printf("\t veuillez introduire la cle que vous voulez inserer\n");
+        scanf("%d",&cle);        // la cle a inserer
+        printf("\t veuillez introduire l'info que vous voulez inserer\n\n");
+        scanf("%s",info);        //info a inserer
+        if(cle_correct(cle))
+           {
+
+            insertion(fichier,cle,info);
+           }
+    }
+}
 
