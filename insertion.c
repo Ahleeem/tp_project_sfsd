8b8b450ed8d811874558e9477f030b5c88e02d4e
Ahleeem <150480223+Ahleeem@users.noreply.github.com>
2024-01-19 11:27:24 +0100

    Update insertion.c

--- insertion.c
+++ insertion.c
@@ -1,24 +1,25 @@
 
 void insertion(Fichier *fichier, int cle, char *info)
 {
-    int i,j,trouv=0,cpt; // declaration des variables
+    int i,j,trouv=0,cpt;
     Buffer buf;
+    
     char *cle_ch=malloc((sizeof(char))*100);            // la chaine qui va contenir l'enregistrement qui va etre inseré
    
-    recherche(fichier,cle,&trouv,&i,&j);          // recherche de la cle pour eviter les  doublon
+    recherche(fichier,cle,&trouv,&i,&j);               // recherche de la cle pour eviter les  doublon
     if(entete(fichier,1)==0)                           // si la cle n'a  pas été trouvé alors on insere dans le premier bloc
       aff_entete(fichier,1,1);
     if(trouv==0)
     {
         cpt=0;
-        i=entete(fichier,3);                                             // positionnelent dans le queue
+        i=entete(fichier,3);                                             // positionnement dans le queue
         j=entete(fichier,4);                                             // positionnnement a la position libre dans le bloc de queue
         liredir(fichier,i,&buf);                                          // lecture du  bloc
         sprintf(cle_ch,"%s","");                                         //convertir en chaine
         concat(cle_ch,cle,info);                                       // creation de l'enregistrement
         ecrire_chaine(fichier,strlen(cle_ch),&i,&j,cle_ch,&cpt,&buf);   // ecriture de la chaine construite
         
-        ecriredir(fichier,i,&buf);                                      // ecriture du dernier bloc en cas de chevauchement
+        //ecriredir(fichier,i,&buf);                                    // ecriture du dernier bloc 
         aff_entete(fichier,3,i);                                        // mise a jour de la quee
         aff_entete(fichier,4,j);                                        // mise a jour de la position libre
     }
@@ -27,14 +28,6 @@ void insertion(Fichier *fichier, int cle, char *info)
         printf(" la cle existe deja\n");
     }
 }
-
-
-/***************/
-//lire inas
-//ecrire may
-//ouvrir may
-/*****************/
-
 
 
 
@@ -160,121 +153,3 @@ void concat(char chaine[], int cle, char info[])  //  a inserer dans le ficheir 
     strcat(chaine,ch_f);                          // construction de la chaine finale avec l'ordre suivant taille efface cle info
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-void Inserer(char *NomFichier, char *Cle, char *Enreg) {
-   
-    Recherche(NomFichier, Cle, &Trouv, &i, &j);
-
-    if (Trouv) {
-        printf("Insertion impossible\n");
-    } 
-    else {
-        FILE *F;
-        bloc buf;
-
-        Ouvrir(F, NomFichier, 'A');
-        i = Entete(F, 2);
-        j = Entete(F, 3);
-
-        sprintf(buf.chaine_longueur, "%03d", (int)strlen(Enreg));//convertir la longeur en chaine de 3 car
-        Ecrire_chaine(3, buf.chaine_longueur, i, j);//copier chaine long dans le buf apartir de j
-        buf.type = 'N';
-        Ecrire_chaine(1, &buf.type, i, j);
-        Ecrire_chaine(strlen(Enreg), Enreg, i, j);
-
-        buf.suiv = -1;
-        EcrireDir(F, i, buf);
-
-        if (i != Entete(F, 2)) {
-            AFF_Entete(F, 2, i);
-        }
-
-        AFF_Entete(F, 3, j);
-
-        Fermer(F);
-    }
-}
-*/
-
-/*
-void inseretion ( )
-{
-bool trouv;
-int i,j,k;
-
-
-
-Ouvrir( F,nomfich, 'A')
-// on commence par rechercher la clé e.cle avec le module précédent pour localiser l'emplacement (i,j)
-// où doit être insérerer dans le fichier.
-i=entete(F,2)
- j=entete(F,3);
-
-Rech( e.cle, nomfich, trouv, i, j )
-
-SI ( Non trouv )         // e doit être inséré dans le bloc i à la position j
-continu ← vrai            // en décalant les enreg j, j+1, j+2, ... vers le bas
-// si i est plein, le dernier enreg de i doit être inséré dans i+1
-
-TQ ( continu et i ≤ entete(F,1) ) // si le bloc i+1 est aussi plein son dernier enreg sera
-
-LireDir( F, i, buf ) // inséré dans le bloc i+2, etc ... donc une boucle TQ.
-// avant de faire les décalages, sauvegarder le dernier enreg dans une var x ...
-x ← buf.tab[ buf.NB ]
-// décalage à l'intérieur de buf ...
-k ← buf.NB
-
-TQ (k > j)
-buf.tab[ k ] ← buf.tab[ k-1 ] ; k ← k-1
-FTQ
-
-// insérer e à la pos j dans buf ...
-buf.tab[ j ] ← e
-// si buf n'est pas plein, on remet x à la pos NB+1 et on s'arrête ...
-
-SI ( buf.NB < b ) // b est la capacité max des blocs (une constante)
-buf.NB ← buf.NB+1 ; buf.tab[ buf.NB ] ← x
-EcrireDir( F, i, buf )
-continu ← faux
-
-SINON // si buf est plein, x doit être inséré dans le bloc i+1 à la pos 1 ...
-EcrireDir( F, i, buf )
-i ← i+1 ; j ← 1
-e ← x // cela se fera (l'insertion) à la prochaine itération du TQ
-FSI // non ( buf.NB < b )
-
-FTQ
-
-// si on dépasse la fin de fichier, on rajoute un nouveau bloc contenant un seul enregistrement e
-SI i > entete( F, 1 )
-buf.tab[1] ← e ; buf.NB ← 1
-EcrireDir( F, i, buf ) // il suffit d'écrire un nouveau bloc à cet emplacement
-Aff-entete( F, 1, i ) // on sauvegarde le num du dernier bloc dans l'entete 1
-FSI
-}
-Aff-entete( F, 2 , entete(F,2)+1 ) // on incrémente le compteur d'insertions
-FSI
-
-Fermer( F )
-FIN // insertion
-*/
